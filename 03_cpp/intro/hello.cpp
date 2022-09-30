c08e0b02090d255ae3ecf7e6c50cf1d0b6e9b418
Alex Orehov <Orehov-Sasha2010@mail.ru>
2022-09-30 22:22:48 +0300

    feat: hello.cpp added

---
+++ 03_cpp/intro/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	string n = "";
+	cout << "What's your name?" << endl;
+	cin >> n;
+	cout << "Hello, " << n << "!" << endl;
+	system("PAUSE");
+	return 0;
+}
