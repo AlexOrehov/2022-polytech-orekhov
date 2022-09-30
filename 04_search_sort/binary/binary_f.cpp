260d2201f24961a4280fd5498cee34da7958315b
Alex Orehov <Orehov-Sasha2010@mail.ru>
2022-10-01 00:16:20 +0300

    feat: project binary added

---
+++ 04_search_sort/binary/binary_f.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int bsearch(int arr[], int s, int e, int x);
+{
+	if (e >= 1)
+	{
+		while (s <= e)
+		{
+			int m = (s + e) / 2;
+			if (arr[m] == x)
+				return m;
+			if (arr[m] < x)
+			{
+				s = m + 1;
+			}
+			if (arr[m] > x)
+			{
+				e = m - 1;
+			}
+		}
+	}
+	return -1;
+}
