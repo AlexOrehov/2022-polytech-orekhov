c743a2be2fe4430c5030c429af9f883207746353
Alex Orehov <Orehov-Sasha2010@mail.ru>
2022-11-18 18:26:34 +0300

    fix: palinrome.cpp updated

--- 06_pointers_references/palindrome/palindrome.cpp
+++ 06_pointers_references/palindrome/palindrome.cpp
@@ -1,33 +1,25 @@
-#include <cassert>
+#include<cassert>
+#include<cstring>
 
-unsigned int length(const char* str)
+bool palindrome_check(const char* s)
 {
-    if (!str)
-        return 0;
-    const char* end = str;
-    for (; *end != '\0'; ++end)
-        ;
-    return end - str;
-}
-
-bool ispalindrome(const char* str)
-{
-    if (!str || !str[0])
-        return false;
-    int len = length(str);
-    for (int i = 0; i < len / 2; ++i)
-    {
-        if (str[i] != str[len - 1 - i])
-            return false;
-    }
-    return true;
+	int k = 0;
+	int n = strlen(s) - 1;
+	for (int i = 0, k = n; i <= k; i++, k--)
+	{
+		if (s[i] != s[k])
+			return false;
+	}
+	return true;
 }
 
 int main()
 {
-    assert(ispalindrome("tenet") == true);
-    assert(ispalindrome("") == false);
-    assert(ispalindrome("hello world") == false);
-    assert(ispalindrome(nullptr) == false);
-    return 0;
+	assert(palindrome_check("b") == true);
+	assert(palindrome_check("uehheu") == true);
+	assert(palindrome_check("super") == false);
+	assert(palindrome_check("repaper") == true);
+	assert(palindrome_check(nullptr) == true);
+	assert(palindrome_check(" ") == true);
+	return  0;
 }
