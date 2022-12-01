b17c425b527237b7a72c4856357642b3b2dd63f4
Alex Orehov <Orehov-Sasha2010@mail.ru>
2022-12-01 20:01:10 +0300

    fix array_f.cpp updated

--- 04_search_sort/intro_sort/array_f.cpp
+++ 04_search_sort/intro_sort/array_f.cpp
@@ -1,6 +1,9 @@
 #include <random>
 #include <iostream>
+
 using namespace std;
+
+//Filling the array
 
 void fill_array_random(int arr[], int n, int a, int b)
 {
@@ -14,12 +17,65 @@ void fill_array_random(int arr[], int n, int a, int b)
     }
 }
 
-void print_array(int arr[], int n, bool show_index = false)
+void swap(int arr[], int idx_a, int idx_b)
 {
+    int temp = arr[idx_a]; // temporary variable
+    arr[idx_a] = arr[idx_b];
+    arr[idx_b] = temp;
+}
 
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << '\t';
-    cout << '\n';
+void selection_sort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        if (min_index != i)
+        {
+            swap(arr, i, min_index);
+        }
+    }
+}
+
+//Output function
+
+void print_array(int arr[], int n, bool show_index = true)
+{
+    selection_sort(arr, n);
+    if (show_index == false)
+    {
+        cout << "{";
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i];
+            while (i < n)
+            {
+                cout << ",";
+                break;
+            }
+        }
+        cout << "}" << endl;
+    }
+    else
+    {
+        cout << "{";
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << ":" << arr[i];
+            while (i < n)
+            {
+                cout << ",";
+                break;
+            }
+        }
+        cout << "}" << endl;
+    }
 }
 
 int main()
@@ -28,5 +84,6 @@ int main()
     int n = sizeof(arr) / sizeof(arr[0]);
     fill_array_random(arr, n, 0, 100);
     print_array(arr, n);
+    system("pause");
     return 0;
 }
